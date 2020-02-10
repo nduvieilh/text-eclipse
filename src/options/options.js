32341569166b024f9070b261efd36b6df531d915
Nicky Duvieilh <nicky.duvieilh@gmail.com>
2020-02-09 21:49:38 -0600

    cleaning up code

--- src/options/options.js
+++ src/options/options.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import App from './App';
 import vuetify from '../plugins/vuetify';
-//import Vuetify from 'vuetify/lib';
 
 global.browser = require('webextension-polyfill');
 
